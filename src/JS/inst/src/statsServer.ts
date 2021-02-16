bb86a61de1de5d8bd1c6a9b45fdf6fcd0b2e609f
Daniel Dostanich <dostanichdaniel@gmail.com>
2021-02-17 00:41:07 +0300

    Added logout

--- src/JS/inst/src/statsServer.ts
+++ src/JS/inst/src/statsServer.ts
@@ -1,6 +1,8 @@
 'use strict';
 
 import ws = require('ws');
+const fs = require('fs-extra');
+import path = require('path');
 
 import {getFollowers, StatsRequest, StatsResponse} from "./stats";
 
@@ -14,11 +16,11 @@ async function getAndSendFollowersCount(socket: any, id: string, timeout: number
     try {
         const usersInfo: StatsResponse = await getFollowers(id);
         const userJSON: string = JSON.stringify(usersInfo);
-        console.log(`Stats: Data sent: ${userJSON.slice(0, 80)}`);
+        console.log(`Stats sent: ${userJSON.slice(0, 80)}`);
         socket.send(Buffer.from(userJSON));
     } catch (e) {
         const errorJSON: string = JSON.stringify(e.message);
-        console.log(`Stats: Error sent: ${errorJSON}`);
+        console.log(`Stats sent: ${errorJSON}`);
         socket.send(Buffer.from(errorJSON));
     }
 
@@ -56,6 +58,37 @@ server.on('connection', function connection(socket) {
                 activeFollowerGetters.delete(request.inst_id);
                 break;
             case 'Logout':
+                try{
+                    if (await fs.pathExists(path.resolve(__dirname, path.resolve(__dirname, `cookies/${request.inst_id}`)))){
+                        await fs.remove(path.resolve(__dirname, path.resolve(__dirname, `cookies/${request.inst_id}`)));
+                        let okResponse: StatsResponse = {
+                            status: true,
+                            inst_id: request.inst_id,
+                        };
+                        let okJSON = JSON.stringify(okResponse);
+                        console.log(`Stats sent: ${okJSON}`);
+                        socket.send(Buffer.from(okJSON));
+                    } else {
+                        let errorObj: StatsResponse = {
+                            status: false,
+                            inst_id: request.inst_id,
+                            errorMessage: `Error: there's no such user.`,
+                        };
+                        let errorJSON = JSON.stringify(errorObj);
+                        console.log(`Stats sent: ${errorJSON}`);
+                        socket.send(Buffer.from(errorJSON));
+                    }
+                }
+                catch (e){
+                    let errorObj: StatsResponse = {
+                        status: false,
+                        inst_id: request.inst_id,
+                        errorMessage: `Failure during logout: ${e.message}`,
+                    };
+                    let errorJSON = JSON.stringify(errorObj);
+                    console.log(`Stats sent: ${errorJSON}`);
+                    socket.send(Buffer.from(errorJSON));
+                }
                 break;
         }
     })
