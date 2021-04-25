637197344fa5d40e73430c734d569a42a101d110
Daniel Dostanich <dostanichdaniel@gmail.com>
2021-04-25 18:26:54 +0300

    Bugfix

--- src/JS/inst/src/login.ts
+++ src/JS/inst/src/login.ts
@@ -83,7 +83,7 @@ export class Login {
         let isCheck: boolean = false;
         let wasError: boolean = false;
         try {
-            await action(username, code);
+            await action.call(this, username, code);
 
             const check = await this.runChecks(checks);
 
