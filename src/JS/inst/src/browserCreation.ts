08bb94a7df1123c855893c08726631c8dd2f6254
Daniel Dostanich <dostanichdaniel@gmail.com>
2021-07-28 22:16:53 +0300

    Added hard-coded proxy

--- src/JS/inst/src/browserCreation.ts
+++ src/JS/inst/src/browserCreation.ts
@@ -1,10 +1,28 @@
 import puppeteer = require('puppeteer');
+import {Logger} from "./log";
+
+export interface Proxy {
+    ip: string,
+    port_http: number,
+    username: string,
+    password: string,
+}
+
+const proxy: Proxy = {
+    ip: '185.81.147.205',
+    port_http: 5986,
+    username: 'user61575',
+    password: 'wk5rx4'
+}
 
 export async function createBrowser(dirPath: string): Promise<puppeteer.Browser> {
     const args = [
         '--no-sandbox',
         '--lang=en-GB',
+        `--proxy-server=${proxy.ip}:${proxy.port_http}`,
     ];
+    Logger.info(`proxy: ${proxy.ip}:${proxy.port_http}`);
+
 
     const browser: puppeteer.Browser = await puppeteer.launch({
         headless: false,
@@ -13,6 +31,11 @@ export async function createBrowser(dirPath: string): Promise<puppeteer.Browser>
     });
 
     const page: puppeteer.Page = (await browser.pages())[0];
+
+    await page.authenticate({
+        username: proxy.username,
+        password: proxy.password,
+    });
 
     await page.waitForTimeout(1500);
 
